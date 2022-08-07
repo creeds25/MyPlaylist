63e9730d4618fb2615ded415d08e955f84668504
creeds25 <adnanbukhari800@gmail.com>
2022-08-07 14:21:44 +0500

    Used another linearlayout to allign two text views on the same line.
    Also completed the part of Moving from login to signup UI After clicking create account.

--- app/src/main/java/com/example/myplaylist/MainActivity.java
+++ app/src/main/java/com/example/myplaylist/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.myplaylist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView create_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        create_account = (TextView) findViewById(R.id.create_account);
+        create_account.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.create_account:
+                startActivity(new Intent(this, Signup.class));
+        }
+
     }
 }
--- app/src/main/java/com/example/myplaylist/Signup.java
+++ app/src/main/java/com/example/myplaylist/Signup.java
@@ -3,17 +3,12 @@ package com.example.myplaylist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-public class Signup extends AppCompatActivity implements View.OnClickListener{
-    private TextView Signup;
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        Signup = (TextView) findViewById(R.id.signup);
-        Signup.setOnClickListener(this);
     }
 }
